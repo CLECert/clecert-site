0b3feb1654696874dafd36d3a6eaf73f1ce3a2bc
Colin Campbell <colin@colincampbell.me>
2015-04-03 11:00:29 -0500

    Create Jurisdiction selection page

--- js/site.js
+++ js/site.js
@@ -1,5 +1,29 @@
 $(function () {
 
-  //
-  
+  var currentStep = $("body > section").data("step");
+
+  var usmap = new Datamap({
+    scope: 'usa',
+    element: document.getElementById('jurisdiction-map'),
+
+    geographyConfig: {
+      popupOnHover: false,
+      highlightFillColor: '#8ad1e5'
+    },
+
+    done: function(datamap) {
+      datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
+          var redirectURL = "../data-entry/";
+          redirectURL += "?jurisdiction=" + geography.id;
+          window.location.href = redirectURL;
+      });
+    },
+
+    fills: {
+      defaultFill: '#5bc0de'
+    },
+  });
+
+  usmap.labels();
+
 });
